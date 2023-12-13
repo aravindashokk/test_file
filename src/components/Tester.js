c3c7997db5aa2d83e269a6951eb8e590b7a9c3fa
aravindashokk <aravindkayal@gmail.com>
2023-12-12 18:40:46 -0600

    size limit for each file

--- src/components/Tester.js
+++ src/components/Tester.js
@@ -1,12 +1,13 @@
-import { Button, Progress, Space, Upload, Typography } from "antd";
+import { Button, Progress, Space, Upload, Typography , message} from "antd";
 import { FileOutlined, CloudUploadOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import { useState } from "react";
-
 const Tester = () => {
 
-    const [files, setfiles] = useState({})
-    const [maxSize, setmaxSize] = useState(false)
+    const [files, setfiles] = useState({});
+    const [errorMessage, setErrorMessage] = useState("");
+
+
     const handleFileUpload = async ({ file }) => {
         // console.log(file);
         const getFileObject = (progress, estimated) => {
@@ -23,17 +24,7 @@ const Tester = () => {
         const formData = new FormData();
         formData.append('file', file);
 
-        const totalSize = Object.values(files).reduce((total, current) => {
-            total += current.size / (1024 * 1024); // convert bytes to MB
-            return total;
-        }, 0);
-
-        // Check if adding the current file exceeds the limit
-        if ((totalSize + file.size / (1024 * 1024)) > 250) {
-            setmaxSize(true);
-            return; // Prevent further processing
-        }
-
+        
 
         const req = await axios.post('http://localhost:8000/fileUpload', formData, {
             headers: {
@@ -57,6 +48,19 @@ const Tester = () => {
         return total
     },0);
 
+    const beforeUpload = (file) => {
+        const maxFileSize = 250 * 1024 * 1024; // 250MB limit
+        if (file.size > maxFileSize) {
+            // Notify the user that the file size exceeds the limit
+            message.error(`${file.name} file upload failed.`);
+            console.error("File size exceeds the limit of 250MB");
+            return false; // Prevent uploading files larger than 250MB
+        }
+        setErrorMessage("");
+        return true; // Allow uploading files smaller than 250MB
+    };
+
+
     const getTimeString = (timeInSeconds) => {
         const hours = Math.floor(timeInSeconds / 3600);
         const minutes = Math.floor(timeInSeconds / 60 - hours * 60);
@@ -73,20 +77,21 @@ const Tester = () => {
 
     return (
         <Space direction="vertical" style={{ width: "100vw", display: "flex", justifyContent: "center", alignItems: "center", marginTop: 134, }}>
+            {errorMessage && (<Typography.Text type="danger">{errorMessage}</Typography.Text>)}
             <Upload.Dragger
                 multiple
                 customRequest={handleFileUpload}
                 showUploadList={false}
+                beforeUpload={beforeUpload}
                 style={{ width: 560, marginRight: 14 }}
-                disabled={maxSize}
+                
             >
                 <p className="ant-upload-drag-icon">
                     <CloudUploadOutlined style={{ fontSize: '80px', color: '#000' }} />
                 </p>
                 Drag and Drop files here OR <Button>Click to Upload</Button>
             </Upload.Dragger>
-            {maxSize ? (<Typography.Text type="danger" style={{marginLeft:-280}}>Total size exceeds 250MB</Typography.Text>) 
-            : (<Typography.Text type="secondary" style={{marginLeft:-280}}>Total size: {totalSize} MB</Typography.Text>)}
+            <Typography.Text type="secondary" style={{marginLeft:-280}}>Total size: {totalSize} MB</Typography.Text>
             
             <div style={{ maxHeight: 190, overflowY: 'scroll', width: 575, paddingRight: 1, scrollbarWidth: 'thin' }}>
                 {Object.values(files).map((file, index) => {
